b8b68863dde96116004d7d7b20d2a6b0bfbf726c
Vitaliy Cross <derdavitaliy@gmail.com>
2020-06-01 21:20:50 +0300

    added some changes

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,5 @@
 'use strict';
 const baseUrl = 'https://pixabay.com/api/';
+const queryString =
+  '?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=';
 const key = '16822291-3bd987bc1a9b2ff27c6ed5ac6';
-
-// '?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ'
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 'use strict';
+
+import createImageCards from '../templates/img-card-temp.hbs';
+import fetchImages from './apiService';
+
+const refs = {
+  gallery: document.querySelector('#gallery'),
+  form: document.querySelector('#search-form'),
+  button: document.querySelector('#load-btn'),
+};
