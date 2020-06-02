f0137c52ef8e30f50d4e6cae54d8a8b7cfe58960
Vitaliy Cross <derdavitaliy@gmail.com>
2020-06-02 22:02:05 +0300

    Added hw-13 image finder

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,5 +1,34 @@
 'use strict';
 const baseUrl = 'https://pixabay.com/api/';
-const queryString =
-  '?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=';
-const key = '16822291-3bd987bc1a9b2ff27c6ed5ac6';
+const keyAPI = '16822291-3bd987bc1a9b2ff27c6ed5ac6';
+
+export default {
+  page: 1,
+  query: '',
+  fetchImages() {
+    const requestParams = `?image_type=photo&orientation=horizontal&q=${this.query}&page=${this.page}&per_page=12&key=${keyAPI}`;
+
+    return fetch(baseUrl + requestParams)
+      .then(response => response.json())
+      .then(parsedResponse => {
+        this.incrementPage();
+        return parsedResponse.hits;
+      });
+  },
+
+  incrementPage() {
+    this.page += 1;
+  },
+
+  resetPage() {
+    this.page = 1;
+  },
+
+  get searchQuery() {
+    return this.query;
+  },
+
+  set searchQuery(str) {
+    this.query = str;
+  },
+};
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,42 @@
 'use strict';
 
+const debounce = require('lodash.debounce');
+
 import createImageCards from '../templates/img-card-temp.hbs';
-import fetchImages from './apiService';
+import apiService from './apiService';
 
 const refs = {
   gallery: document.querySelector('#gallery'),
   form: document.querySelector('#search-form'),
   button: document.querySelector('#load-btn'),
+  // input: document.querySelector('input[name="query"]'),
 };
+
+refs.form.addEventListener('submit', searchFromInputHandler);
+refs.button.addEventListener('click', loadMoreButtonHandler);
+
+function searchFromInputHandler(e) {
+  e.preventDefault();
+
+  const inputValue = e.currentTarget.elements.query.value;
+
+  clearListItems();
+
+  apiService.resetPage();
+  apiService.searchQuery = inputValue;
+  apiService.fetchImages().then(insertListItems);
+}
+
+function loadMoreButtonHandler() {
+  apiService.fetchImages().then(insertListItems);
+}
+
+function insertListItems(items) {
+  const markup = createImageCards(items);
+
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearListItems() {
+  refs.gallery.innerHTML = '';
+}
