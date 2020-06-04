97551486fe45b4554866f091d8084d71e63c2b7d
Vitaliy Cross <derdavitaliy@gmail.com>
2020-06-04 14:08:05 +0300

    added hw-13 image finder

--- src/js/app.js
+++ src/js/app.js
@@ -9,11 +9,15 @@ const refs = {
   gallery: document.querySelector('#gallery'),
   form: document.querySelector('#search-form'),
   button: document.querySelector('#load-btn'),
+  modalWindow: document.querySelector('.js-lightbox'),
+  lightboxImage: document.querySelector('.lightbox__image'),
   // input: document.querySelector('input[name="query"]'),
 };
 
 refs.form.addEventListener('submit', searchFromInputHandler);
 refs.button.addEventListener('click', loadMoreButtonHandler);
+// refs.gallery.addEventListener('click', showModalWindowHandler);
+// refs.modalWindow.addEventListener('click', closeModalWindow);
 
 function searchFromInputHandler(e) {
   e.preventDefault();
@@ -40,3 +44,30 @@ function insertListItems(items) {
 function clearListItems() {
   refs.gallery.innerHTML = '';
 }
+
+// function showModalWindowHandler(e) {
+//   if (e.target.id !== 'js-icon-fullscreen') {
+//     return;
+//   }
+
+//   // console.dir(e.target);
+//   // console.log(e.currentTarget);
+//   // const url = e.target.dataset.source;
+//   // const alt = e.target.getAttribute('alt');
+//   refs.modalWindow.classList.add('is-open');
+//   // lightboxImage.setAttribute('src', `${url}`);
+//   // lightboxImage.setAttribute('alt', `${alt}`);
+// }
+
+// function closeModalWindow(e) {
+//   if (
+//     e.target.tagName !== 'BUTTON' &&
+//     e.target.parentNode !== e.currentTarget
+//   ) {
+//     return;
+//   }
+
+//   refs.modalWindow.classList.remove('is-open');
+//   refs.lightboxImage.removeAttribute('src');
+//   refs.lightboxImage.removeAttribute('alt');
+// }
